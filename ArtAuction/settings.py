753bc352f44041e1aba0c2a004fb01010eea6700
chinmay270876 <123950009+chinmay270876@users.noreply.github.com>
2024-04-30 21:42:51 +0530

    Update settings.py

--- ArtAuction/settings.py
+++ ArtAuction/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_t^v#e^u+=yu^af+a5s4f#2@9*2^cz$6aiq)zeizbfx$l$s9p*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
